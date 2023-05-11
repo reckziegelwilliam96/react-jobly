5f2fb3ed40c2b3e6e4b943134f7bb86c089037fd
Liam Reckziegel <liamreckziegel@Liams-iMac.local>
2023-05-11 10:25:28 -0700

    commit index.js changes

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -6,11 +6,11 @@ import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.render(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+ReactDOM.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>,
+  document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
